6b31051a6b2f3dd6fd8108a8198ff156f0af0eda
Kristine Sonu <mnyo@kakao.com>
2021-06-08 00:00:57 +0900

    character movement components added

---
+++ dontpanic/src/components/player/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Actor from "../actor";
+import useKeyPress from "../../hooks/use-key-press";
+import useWalk from "../../hooks/use-walk";
+
+export default function Player({ skin }) {
+    const { dir, step, walk, position } = useWalk(3)
+    const data = {
+        h: 32,
+        w: 32,
+      };
+
+    useKeyPress((e) => {
+        walk(e.key.replace("Arrow", "").toLowerCase());
+        e.preventDefault();
+    })
+
+    return <Actor
+    sprite={`./${skin}.png`}
+    data={ data }
+    step={step}
+    dir={dir}
+    position={position}
+    />;
+}
